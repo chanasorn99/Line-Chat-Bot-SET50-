d5e834c375cbea0ad6514c56a545c88c0cd54516
Chanasorn <59010889+chanasorn99@users.noreply.github.com>
2019-12-19 16:41:29 +0700

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,7 @@
+Channel_secret = "923c6b5493725b77ec68b271d57e9761"
+
+
+Channel_access_token = "dg5joHe2OivS6EhNWn0w9U0JkavSOg79A9FGtJhmAkBui4/rKZ8Nn8roEpL4r1kpu98rhdQJsnWYMdr93/3hKSI2ybkpgMBV7xECLAHIGCB91gskQ+Iu6IRFN/EwRgOIB7dgq35Zp5bDij5b27vEBwdB04t89/1O/w1cDnyilFU="
+
+
+Basic_ID = "@256qadkd"
